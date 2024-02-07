d5c9b7df6971020321bbe096e486d88f85f8673f
omararabi100 <omararabi100@gmail.com>
2024-02-07 16:14:41 +0200

    started contact us

--- medtech/src/components/ContactUs.jsx
+++ medtech/src/components/ContactUs.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const ContactUs = () => {
     return (
-        <div>
-            Contact Us
+        <div className="contact-us-container">
+            <form action="">
+                <input type="text" placeholder="Email"/>
+                <input type="text" placeholder="First Name"/>
+                <input type="text" placeholder="Last Name"/>
+                <textarea cols="30" rows="10"></textarea>
+            </form>
         </div>
     )
 }
